4b70d24b590596027c0357fbc13fdb25a4cf7d02
Maryannexe <maryannexe@Maryannexes-MacBook-Pro.local>
2017-05-22 14:52:25 -0400

    Added Sandbox website

---
+++ sandbox/js/script.js
@@ -0,0 +1,14 @@
+﻿jQuery(function(){
+  var menuOffset = jQuery('#site-navigation')[0].offsetTop;
+  jQuery(document).bind('ready scroll',function() {
+    var docScroll = jQuery(document).scrollTop();
+    if(docScroll >= menuOffset) {
+      jQuery('#site-navigation').addClass('fixed');
+    } else {
+      jQuery('#site-navigation').removeClass('fixed');
+    }
+   });
+});
+
+
+
